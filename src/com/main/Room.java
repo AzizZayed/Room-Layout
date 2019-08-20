6df613c82c91684ff875c8914c387991cabb8532
Abd-El-Aziz Zayed <48853777+AzizZayed@users.noreply.github.com>
2019-08-20 18:04:59 -0400

    first commit

---
+++ src/com/main/Room.java
@@ -0,0 +1,257 @@
+package com.main;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Room {
+
+	private int realWidth, realHeight, width, height;
+	private float scale = 5.0f;
+	private boolean checkForFurnitureCollisions = false;
+	private boolean checkForBorderCollisions = false;
+	private boolean closetDoorsOpen = true;
+	private Rectangle closedDoor, openDoor, window, closetDoors;
+	private ArrayList<Polygon> initialState, mobileFurniture;
+	private ArrayList<Color> furnitureColors;
+	private int furnitureSelected = -1;
+	private int px, py;
+
+	private boolean doorOpen = false;
+
+	public Room(int w, int h) {
+		realWidth = w;
+		realHeight = h;
+		width = (int) Math.ceil(w * scale);
+		height = (int) Math.ceil(h * scale);
+
+		initialState = new ArrayList<Polygon>();
+		mobileFurniture = new ArrayList<Polygon>();
+		furnitureColors = new ArrayList<Color>();
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getRealWidth() {
+		return realWidth;
+	}
+
+	public int getRealHeight() {
+		return realHeight;
+	}
+
+	private Color getRandomColor() {
+		Random rand = new Random();
+
+		int r = 0, g = 0, b = 0;
+		final int min = 25, max = 256;
+		;
+
+		while (r < min) {
+			r = rand.nextInt(max);
+		}
+		while (g < min) {
+			g = rand.nextInt(max);
+		}
+		while (b < min) {
+			b = rand.nextInt(max);
+		}
+
+		return new Color(r, g, b);
+	}
+
+	public void addFurniture(float[] xpoints, float[] ypoints, int npoints) {
+
+		int[] x = new int[npoints];
+		int[] y = new int[npoints];
+
+		for (int i = 0; i < xpoints.length; i++) {
+			x[i] = (int) Math.ceil(xpoints[i] * scale);
+		}
+		for (int i = 0; i < ypoints.length; i++) {
+			y[i] = (int) Math.ceil(ypoints[i] * scale);
+		}
+
+		initialState.add(new Polygon(x, y, npoints));
+		mobileFurniture.add(new Polygon(x, y, npoints));
+		furnitureColors.add(getRandomColor());
+	}
+
+	public void addClosedDoor(float dx, float dy, float dw, float dh) {
+		int x, y, w, h;
+
+		x = (int) Math.ceil(dx * scale);
+		y = (int) Math.ceil(dy * scale);
+		w = (int) Math.ceil(dw * scale);
+		h = (int) Math.ceil(dh * scale);
+
+		closedDoor = new Rectangle(x, y, w, h);
+	}
+
+	public void addOpenDoor(float dx, float dy, float dw, float dh) {
+		int x, y, w, h;
+
+		x = (int) Math.ceil(dx * scale);
+		y = (int) Math.ceil(dy * scale);
+		w = (int) Math.ceil(dw * scale);
+		h = (int) Math.ceil(dh * scale);
+
+		openDoor = new Rectangle(x, y, w, h);
+	}
+
+	public void addClosetDoors(float dx, float dy, float dw, float dh) {
+		int x, y, w, h;
+
+		x = (int) Math.ceil(dx * scale);
+		y = (int) Math.ceil(dy * scale);
+		w = (int) Math.ceil(dw * scale);
+		h = (int) Math.ceil(dh * scale);
+
+		closetDoors = new Rectangle(x, y, w, h); // closet doors when open
+	}
+
+	public void addWindow(float dx, float dy, float dw, float dh) {
+		int x, y, w, h;
+
+		x = (int) Math.ceil(dx * scale);
+		y = (int) Math.ceil(dy * scale);
+		w = (int) Math.ceil(dw * scale);
+		h = (int) Math.ceil(dh * scale);
+
+		window = new Rectangle(x, y, w, h);
+	}
+
+	public void press(int x, int y) {
+		
+		if (doorOpen)
+			doorOpen = !openDoor.contains(x, y);
+		else
+			doorOpen = closedDoor.contains(x, y);
+		
+		boolean clicked = false;
+		int i = mobileFurniture.size() - 1;
+
+		while (i >= 0 && !clicked) {
+			Polygon poly = mobileFurniture.get(i);
+			if (poly.contains(x, y)) {
+				clicked = true;
+				furnitureSelected = i;
+			}
+			i--;
+		}
+
+		px = x;
+		py = y;
+	}
+
+	public void drag(int x, int y) {
+
+		if (furnitureSelected < 0)
+			return;
+
+		int dx = x - px;
+		int dy = y - py;
+
+		Polygon poly = mobileFurniture.get(furnitureSelected);
+		poly.translate(dx, dy);
+
+		boolean furnitureColision = false;
+		if (checkForFurnitureCollisions) {
+			int i = 0;
+
+			while (i < mobileFurniture.size() && !furnitureColision) {
+				if (i != furnitureSelected) {
+					furnitureColision = poly.getBounds().intersects(mobileFurniture.get(i).getBounds());
+				}
+				i++;
+			}
+		}
+
+		if (checkForBorderCollisions) {
+			Rectangle fullRoom = new Rectangle(0, 0, width, height);
+			if (!fullRoom.contains(poly.getBounds()) || furnitureColision || poly.intersects(closetDoors.getBounds())) {
+				poly.translate(-dx, -dy);
+			}
+		}
+
+		px = x;
+		py = y;
+
+	}
+
+	public void release() {
+		furnitureSelected = -1;
+	}
+
+	public void rotateSelected(double angle) {
+
+		if (furnitureSelected < 0)
+			return;
+
+		mobileFurniture.set(furnitureSelected, rotatePolygon(furnitureSelected, angle));
+	}
+
+	private Polygon rotatePolygon(int index, double angle) {
+
+		Polygon polygon = mobileFurniture.get(index);
+		Point.Double center = new Point.Double(polygon.getBounds().getCenterX(), polygon.getBounds().getCenterY());
+		Polygon newPolygon = new Polygon();
+
+		double sin = Math.sin(angle);
+		double cos = Math.cos(angle);
+
+		for (int i = 0; i < polygon.npoints; i++) {
+			Point p = new Point(polygon.xpoints[i], polygon.ypoints[i]);
+
+			double x = p.x * cos - p.y * sin + center.x * (1 - cos) + center.y * sin;
+			double y = p.x * sin + p.y * cos + center.y * (1 - cos) - center.x * sin;
+
+			Point.Double point = new Point.Double(x, y);
+
+			newPolygon.addPoint((int) Math.ceil(point.x), (int) Math.ceil(point.y));
+		}
+
+		return newPolygon;
+	}
+
+	public void reset() {
+		mobileFurniture.clear();
+		for (int i = 0; i < initialState.size(); i++) {
+			Polygon poly = initialState.get(i);
+			mobileFurniture.add(new Polygon(poly.xpoints, poly.ypoints, poly.npoints));
+		}
+	}
+
+	public void draw(Graphics2D g2d) {
+
+		for (int i = 0; i < mobileFurniture.size(); i++) {
+			g2d.setColor(furnitureColors.get(i));
+			g2d.fill(mobileFurniture.get(i));
+		}
+
+		g2d.setColor(Color.BLUE);
+		g2d.fill(window);
+
+		g2d.setColor(Color.GREEN);
+		if (doorOpen)
+			g2d.fill(openDoor);
+		else
+			g2d.fill(closedDoor);
+		if (!closetDoorsOpen)
+			return;
+
+		g2d.setColor(Color.DARK_GRAY);
+		g2d.fill(closetDoors);
+	}
+
+}
---
+++ src/com/main/RoomManager.java
@@ -0,0 +1,144 @@
+package com.main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class RoomManager extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private Room room;
+
+	public RoomManager() {
+
+		int roomW = 142;
+		int roomH = 159;
+
+		room = new Room(roomW, roomH);
+		Dimension size = new Dimension(room.getWidth(), room.getHeight());
+
+		this.setPreferredSize(size);
+		this.setMaximumSize(size);
+		this.setMinimumSize(size);
+
+		this.setFocusable(true);
+
+		initRoom();
+
+		this.addMouseWheelListener(new MouseWheelListener() {
+
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				double step = 45.0d * Math.PI / 180.0d;
+				room.rotateSelected(step);
+				repaint();
+			}
+
+		});
+
+		this.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				room.release();
+				repaint();
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				room.press(e.getX(), e.getY());
+				repaint();
+			}
+
+		});
+
+		this.addMouseMotionListener(new MouseMotionAdapter() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				room.drag(e.getX(), e.getY());
+				repaint();
+			}
+
+		});
+
+		this.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int keyCode = e.getKeyCode();
+
+				if (keyCode == KeyEvent.VK_R)
+					room.reset();
+
+				repaint();
+			}
+
+		});
+	}
+
+	private void initRoom() {
+
+		int nObjects = 5;
+
+		float[][] x = { { 33.5f, 94.5f, 94.5f, 33.5f }, // bed
+				{ 95, 141.25f, 141.25f, 116.75f, 116.75f, 95 }, // desk
+				{ 120, 141.5f, 141.5f, 120 }, // shelf
+				{ 12.25f, 33, 33, 12.25f }, // bedside table
+				{ 0, 10, 10, 0 }, // lamp
+		};
+		float[][] y = { { 1, 1, 75, 75 }, // bed
+				{ 0.5f, 0.5f, 67, 67, 27.5f, 27.5f }, // desk
+				{ 120, 120, 158.5f, 158.5f }, // shelf
+				{ 4.5f, 4.5f, 21.5f, 21.5f }, // bedside table
+				{ 0, 0, 10, 10 }, // lamp
+		};
+
+		for (int i = 0; i < nObjects; i++) {
+			room.addFurniture(x[i], y[i], x[i].length);
+		}
+
+		room.addClosedDoor(0, 123.5f, 1, 36.5f);
+		room.addOpenDoor(0, 154f, 30, 1);
+		room.addWindow(141f, 51.25f, 1, 59);
+		room.addClosetDoors(0, 16.75f, 11, 60);
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		drawBackground(g);
+		room.draw((Graphics2D) g);
+	}
+
+	private void drawBackground(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, room.getWidth(), room.getHeight());
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("My Room");
+		RoomManager roomManager = new RoomManager();
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.add(roomManager);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		roomManager.setDoubleBuffered(true);
+
+		roomManager.repaint();
+	}
+
+}
